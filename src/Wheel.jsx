d2404544cd855988dbe69d1ec80ae6620ec5d175
michael-bocim <mtbocim@gmail.com>
2023-10-09 17:31:01 -0600

    Improved css for better styling and flexiblity

--- src/Wheel.jsx
+++ src/Wheel.jsx
@@ -9,7 +9,6 @@ function Wheel() {
   const [currentAngle, setCurrentAngle] = useState(325);
   console.log("What is rotationAngle", currentAngle);
   function setWheelRotation(newAngle) {
-
     const clockwise = (newAngle - currentAngle + 72000) % 360;
     const counterClockwise = (currentAngle - newAngle + 72000) % 360;
 
@@ -30,38 +29,52 @@ function Wheel() {
           transition: "transform 0.5s ease",
         }}
       >
-        {/* <OuterRing /> */}
-        {/* <MiddleRing /> */}
+        <OuterRing />
+        <MiddleRing />
         <InnerRing />
         <div className="Wheel-center" />
-        <div
-          className="Wheel-sad"
-          onMouseDown={() => setWheelRotation(31)}
-        ></div>
-        <div
-          className="Wheel-disgusted"
-          onMouseDown={() => setWheelRotation(75)}
-        ></div>
-        <div
-          className="Wheel-angry"
-          onMouseDown={() => setWheelRotation(125)}
-        ></div>
-        <div
-          className="Wheel-fearful"
-          onMouseDown={() => setWheelRotation(189)}
-        ></div>
-        <div
-          className="Wheel-bad"
-          onMouseDown={() => setWheelRotation(232)}
-        ></div>
-        <div
-          className="Wheel-surprised"
-          onMouseDown={() => setWheelRotation(268)}
-        ></div>
-        <div
-          className="Wheel-happy"
-          onMouseDown={() => setWheelRotation(325)}
-        ></div>
+        <div className="Wheel-rotation-container rotation-sad">
+          <div
+            className="rotation-selector-sad"
+            onMouseDown={() => setWheelRotation(31)}
+          ></div>
+        </div>
+        <div className="Wheel-rotation-container rotation-disgusted">
+          <div
+            className="rotation-selector-disgusted"
+            onMouseDown={() => setWheelRotation(75)}
+          ></div>
+        </div>
+        <div className="Wheel-rotation-container rotation-angry">
+          <div
+            className="rotation-selector-angry"
+            onMouseDown={() => setWheelRotation(125)}
+          ></div>
+        </div>
+        <div className="Wheel-rotation-container rotation-fearful">
+          <div
+            className="rotation-selector-fearful"
+            onMouseDown={() => setWheelRotation(189)}
+          ></div>
+        </div>
+        <div className="Wheel-rotation-container rotation-bad">
+          <div
+            className="rotation-selector-bad"
+            onMouseDown={() => setWheelRotation(232)}
+          ></div>
+        </div>
+        <div className="Wheel-rotation-container rotation-surprised">
+          <div
+            className="rotation-selector-surprised"
+            onMouseDown={() => setWheelRotation(268)}
+          ></div>
+        </div>
+        <div className="Wheel-rotation-container rotation-happy">
+          <div
+            className="rotation-selector-happy"
+            onMouseDown={() => setWheelRotation(325)}
+          ></div>
+        </div>
       </div>
     </div>
   );
